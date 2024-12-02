03762d08d3784abab81ee5618168a27baa26a7c0
evaprashanti <168903097+evaprashanti@users.noreply.github.com>
2024-12-02 17:10:54 +0700

    Create third_page.dart

---
+++ third_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ThirdPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Halaman Ketiga'),
+      ),
+      body: Center(
+        child: Text('Ini adalah Halaman Ketiga'),
+      ),
+    );
+  }
+}
